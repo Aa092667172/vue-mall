89c5092b943612d031566814f570cf391fd46c9f
Aa092667172 <ttaehee520@gmail.com.tw>
2022-05-15 19:02:02 +0800

    SearchButton發API更新mallHome Data

--- src/components/searchButton.vue
+++ src/components/searchButton.vue
@@ -1,41 +1,23 @@
 <template>
      <div>
         <el-col :offset="6" :span="10" >
-          <el-input v-model="inputValue" placeholder="请输入内容"/>
+          <el-input :value="value" @input="$emit('input', $event)" placeholder="请输入内容"/>
         </el-col>
         <el-col :span="2" >
-          <el-button type="button" @click ="selectKeyWord" icon="el-icon-search">搜索</el-button>
+          <el-button type="button" @click ="$emit('click')" icon="el-icon-search">搜索</el-button>
         </el-col>
     </div>
 </template>
 
 <script>
 
-import axios from 'axios'
-
 export default {
   data () {
     return {
-      inputValue: '',
-      total: ''
+      searchValue: ''
     }
   },
-  methods: {
-    selectKeyWord () {
-      try {
-        axios.get('http://localhost:8081/products', {
-          params:
-            { search: this.inputValue }
-        }
-        ).then(res => {
-          this.total = res.data
-          console.log(this.searchData)
-        })
-      } catch (error) {
-        console.log(error)
-      }
-    }
-  }
+  props: ['value']
 }
 </script>
 
